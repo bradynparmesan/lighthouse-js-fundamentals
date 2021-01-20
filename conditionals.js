ac77bf2a3ab95d93db594f193519bb0393be0637
Bradyn Parisian <bradyn.parisian@gmail.com>
2021-01-20 03:05:25 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,16 +1,23 @@
-const rain = false;
-const temperature = 30;
+//const rain = false;
+//const temperature = 30;
 
-if (rain) {
-  console.log("Don't forget your umbrella!");
-}
+//if (!rain) {
+//  console.log("Leave your umbrella at home!");
+//}
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Shore sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
+//if (temperature < -40 || temperature > 40) {
+//  console.log("Maybe going outside isn't such a great idea...");
+//} else if (temperature < 15) {
+//  console.log("Shore sleeves won't cut it!");
+//} else {
+//  console.log("Short sleeves are fine.");
+//}
 
-console.log("Now you're ready to go outside!");
+//console.log("Now you're ready to go outside!");
+
+//const isCitizen = true;
+//const age = 26;
+
+//if (isCitizen && age > 18) {
+//  console.log("You are eligible to vote.");
+//}
