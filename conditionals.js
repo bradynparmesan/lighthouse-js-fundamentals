2db3f591c11669160e150db694934fc927955a75
vagrant <Bradyn Parisian>
2021-01-21 18:17:12 +0000

    Commit hello function

--- conditionals.js
+++ conditionals.js
@@ -21,3 +21,13 @@
 //if (isCitizen && age > 18) {
 //  console.log("You are eligible to vote.");
 //}
+const age = 10;
+const whichSchool  = function (age) {
+  if (age < 13) {
+    console.log("Elementary School");
+  } else if (age >= 13 && age <= 18) {
+    console.log("Secondary School");
+  } else {
+    console.log("Lighthouse Labs");
+  }
+};
