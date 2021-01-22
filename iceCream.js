7a38cf9b331c2b1ea1f4947e137af20c89faa782
Bradyn Parisian <bradyn.parisian@gmail.com>
2021-01-22 22:37:59 +0000

    Array manipulation - ice cream

---
+++ iceCream.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+let arrayLength = iceCreamFlavours.length - 1;
+console.log(iceCreamFlavours[arrayLength]);
