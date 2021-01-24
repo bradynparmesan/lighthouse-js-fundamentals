fe0b96f207721d17b0288dd964184ccd0e256abf
Bradyn Parisian <bradyn.parisian@gmail.com>
2021-01-24 22:42:51 +0000

    function to examine longest name in characters, and return longest name

--- Kata4.js
+++ Kata4.js
@@ -1,22 +1,23 @@
-var instructors =  [{name: "Santa", course: "iOS"},
-{name: "Jeremiah", course: "Web"},
-{name: "Ophilia", course: "Web"},
-{name: "Donald", course: "Web"}]
+/* Function to examine the length of a course instructor's name and return the longest name in the batch */
 
 
-
-
-const instructorWithLongestName = function (instructors) {
+const instructorWithLongestName = (instructors) => {
+  
   let maxIndex = 0; //store index
-  let temp = 0; //initialize and store the length of the name
+  let tempName = 0; //initialize and store the length of the name
+  
   for (let i = 0; i < instructors.length; i++) {
-  if (temp < instructors[i].name.length) {
-  temp = instructors[i].name.length; //if the name length is longer than the current length, assign current length to temp and compare until the longest is found
-  maxIndex = i; //assign index which the longest name is at
+    if (tempName < instructors[i].name.length) {
+      tempName = instructors[i].name.length; 
+      /*if the name length is longer than the current length, assign current length to temp and compare until the longest is found */
+
+      //assign index with the position of the longest name
+      maxIndex = i; 
+    }
   }
-  }
-  return (instructors[maxIndex]); //return the object with index
-}
+  //return the object with index
+  return (instructors[maxIndex]); 
+};
 
 console.log(instructorWithLongestName([
   {name: "Samuel", course: "iOS"},
