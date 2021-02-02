901fd912b33a53da53f5b63ce25ae239d3557121
Bradyn Parisian <bradyn.parisian@gmail.com>
2021-02-02 04:12:06 +0000

    EsLint worthy code

--- w1/reverse.js
+++ w1/reverse.js
@@ -1,9 +1,9 @@
 for (let j = 2; j < process.argv.length; j++) {
   let stringNow = "";
   stringNow += process.argv[j];
-    let reverseString = "";
-    for (let i = stringNow.length - 1; i >= 0; i--) {
-      reverseString += stringNow.charAt(i);
-    }
-    console.log(reverseString);
-};
+  let reverseString = "";
+  for (let i = stringNow.length - 1; i >= 0; i--) {
+    reverseString += stringNow.charAt(i);
+  }
+  console.log(reverseString);
+}
