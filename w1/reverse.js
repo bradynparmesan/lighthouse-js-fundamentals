1550369f73c218f02d8a43cb873d53a378e9ea65
Bradyn Parisian <bradyn.parisian@gmail.com>
2021-02-02 04:07:57 +0000

    Program to output reverse of input arguments from command line using Node.js

---
+++ w1/reverse.js
@@ -0,0 +1,10 @@
+'use strict';
+for (let j = 2; j < process.argv.length; j++) {
+  let stringNow = "";
+  stringNow += process.argv[j];
+    let reverseString = "";
+    for (let i = stringNow.length - 1; i >= 0; i--) {
+      reverseString += stringNow.charAt(i);
+    }
+    console.log(reverseString);
+};
